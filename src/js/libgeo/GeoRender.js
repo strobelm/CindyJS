80416e06eb03301ed6f2f14a208575ad36afdd58
Michael Strobel <strobel@ma.tum.de>
2015-10-29 12:25:35 +0100

    fixed segments with moving endpoints

--- src/js/libgeo/GeoRender.js
+++ src/js/libgeo/GeoRender.js
@@ -68,8 +68,10 @@ function render() {
                 alpha: el.alpha
             });
         } else if (el.clip.value === "end") {
+            pt1 = csgeo.csnames[el.args[0]];
+            pt2 = csgeo.csnames[el.args[1]];
             evaluator.draw$2(
-                [el.startpos, el.endpos], {
+                [pt1.homog, pt2.homog], {
                     size: el.size,
                     color: el.color,
                     alpha: el.alpha
