372e9582c762c71e8796c6366860007451c63ee3
Michael Strobel <strobel@ma.tum.de>
2018-06-18 08:03:11 +0200

    indexof -> hashmap

--- src/js/libgeo/Prover.js
+++ src/js/libgeo/Prover.js
@@ -46,7 +46,7 @@ function applyIncidence(a, b) {
 function incidentPL(p, l) {
     return {
         getInvolved: function() {
-            return [p, l];
+            return [p.name, l.name];
         },
         toString: function() {
             return "point " + p.name + " incident line " + l.name;
@@ -64,7 +64,7 @@ function incidentPL(p, l) {
 function incidentPC(p, c) {
     return {
         getInvolved: function() {
-            return [p, c];
+            return [p.name, c.name];
         },
         toString: function() {
             return "point " + p.name + " incident conic " + c.name;
@@ -92,39 +92,46 @@ function checkConjectures() {
     var involved;
 
     var recalcInvolved = function() {
-        involved = [];
+        involved = {};
         conjectures.forEach(function(con) {
             var invs = con.getInvolved();
             var incis;
             invs.forEach(function(el) {
-                if (involved.indexOf(el) < 0) {
-                    involved.push(el);
+//            for(el in invs){
+                if (!involved[el]) {
+                    involved[el] = true;
                     // also add incidences of involved objects
-                    incis = findAllIncis(el, []);
-                    incis.forEach(function(i) {
-                        if (involved.indexOf(i) < 0) involved.push(i);
-                    });
+                    incis = findAllIncis(el, {});
+ //                   incis.forEach(function(i) {
+                    for(i in incis){
+                        if (!involved[el]) involved[i] = true;
+                    };
                 }
             });
         });
     };
 
     // recursively find all incidences to an geo object
-    var findAllIncis = function(el, list) {
+    var findAllIncis = function(name, map) {
         // get all incidences we don't already know
-        var nincis = el.incidences.filter(function(iels) {
-            return list.indexOf(csgeo.csnames[iels]) < 0;
-        });
-        if (nincis.length === 0) return list;
+        //var nincis = el.incidences.filter(function(iels) {
+        //    return !map[iels.name];
+        //});
+        //if (nincis.length === 0) return map;
         // add new incidences
-        nincis.forEach(function(ii) {
-            list.push(csgeo.csnames[ii]);
-        });
+        //nincis.forEach(function(ii) {
+        //    map[ii] = true;
+        //});
+        var el = csgeo.csnames[name];
+        var len = Object.keys(map).length;
+//        for(i in el.incidences) map[ii] = true;
+        el.incidences.forEach(function(ii){ map[ii]= true;});
+        if(len === Object.keys(map).length) return map;
         // recursive call
-        nincis.forEach(function(nel) {
-            return findAllIncis(csgeo.csnames[nel], list);
-        });
-        return list;
+        //nincis.forEach(function(nel) {
+        for(nel in nincis) findAllIncis(nel, map);
+        //});
+        return map;
     };
 
     recalcInvolved();
@@ -135,19 +142,24 @@ function checkConjectures() {
     };
 
     // add defining elements 
-    involved.forEach(function(inv) {
-        var n = inv.args;
+    //involved.forEach(function(inv) {
+    for(var inv in involved){
+        console.log(inv);
+        var args = csgeo.csnames[inv].args;
         if (typeof(n) === 'undefined') return;
-        n.forEach(function(name) {
-            if (involved.indexOf(csgeo.csnames[name]) < 0)
-                involved.push(csgeo.csnames[name]);
-        });
-    });
+        for(arg in args) involved[arg] = true;
+//        n.forEach(function(name) {
+//            if (involved.indexOf(csgeo.csnames[name]) < 0)
+//                involved.push(csgeo.csnames[name]);
+//        });
+    };
 
     var emove, nconject = conjectures.length;
     for (var kk = 0; kk < nummoves; kk++) {
-        for (var oo = 0; oo < involved.length; oo++) {
-            var el = involved[oo];
+        for(name in involved){
+            var el = csgeo.csnames[name];
+        //for (var oo = 0; oo < involved.length; oo++) {
+        //    var el = involved[oo];
             if (!el.pinned && geoOps[el.type].isMovable) {
                 if (debug) console.log("prover: moving element", el.name);
                 // get random move and move free element
