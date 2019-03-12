d1b75b0839b5bdee90e7e55354302dfc76b9b6e7
Michael Strobel <strobel@ma.tum.de>
2019-03-12 10:58:29 +0100

    beautify

--- src/js/libcs/PSLQ.js
+++ src/js/libcs/PSLQ.js
@@ -1,19 +1,18 @@
 class PSLQMatrix {
     constructor(m) {
-		if(m) {
-			this._e = [];
-        	this._e[0] = m[0][0];
-        	this._e[1] = m[1][0];
-        	this._e[2] = m[2][0];
-        	this._e[3] = m[0][1];
-        	this._e[4] = m[1][1];
-        	this._e[5] = m[2][1];
-        	this._e[6] = m[0][2];
-        	this._e[7] = m[1][2];
-        	this._e[8] = m[2][2];
-		}
-		else{
-			this._e = [1, 0, 0, 0, 1, 0, 0, 0, 1];
-		}
+        if (m) {
+            this._e = [];
+            this._e[0] = m[0][0];
+            this._e[1] = m[1][0];
+            this._e[2] = m[2][0];
+            this._e[3] = m[0][1];
+            this._e[4] = m[1][1];
+            this._e[5] = m[2][1];
+            this._e[6] = m[0][2];
+            this._e[7] = m[1][2];
+            this._e[8] = m[2][2];
+        } else {
+            this._e = [1, 0, 0, 0, 1, 0, 0, 0, 1];
+        }
     }
 }
