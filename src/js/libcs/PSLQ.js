a92af0c048f35a0c23844bf7d40b9e3b2a228323
Michael Strobel <mstrobel@biodataanalysis.de>
2019-03-12 16:08:56 +0100

    finish pslqmatrix

--- src/js/libcs/PSLQ.js
+++ src/js/libcs/PSLQ.js
@@ -1,4 +1,5 @@
 class PSLQMatrix {
+    // overloaded constructor
     constructor(m) {
         if (m) {
             this._e = [];
@@ -20,8 +21,24 @@ class PSLQMatrix {
         return this._e[idx];
     }
 
-    exchange(p1, p2) {
-        [this._e[p1], this._e[p2]] = [this._e[p2], this._e[p1]];
+    getRow(i) {
+        let _e = this._e;
+        return [_e[i], _e[3 + i], _e[6 + i]];
+    }
+
+    // overloaded
+    // 1-ary set internal matrix _e with i
+    // 2-ary set index i to value
+    set(i, value) {
+        if (arguments.length === 1)
+            this._e = i;
+        if (arguments.length === 2)
+            this._e[i] = value;
+    }
+
+    // swap values in internal matrix
+    exchange(idx1, idx2) {
+        [this._e[idx1], this._e[idx2]] = [this._e[idx2], this._e[idx1]];
     }
 
     invert() {
@@ -34,10 +51,57 @@ class PSLQMatrix {
         ];
 
         if (Math.abs(det) < 1e-10) {
-            console.log("PSLQ: inverting singular matrix!")
+            console.log("PSLQ: inverting singular matrix!");
         }
         this._e = res.map(e => e / det);
         return this;
+    }
+
+    swRow(a, b) {
+        let offset = 0;
+        while (offset < 9) {
+            this.exchange(offset + a, offset + b);
+            offset += 3;
+        }
+    }
+
+    swCol(a, b) {
+        for (let i = 0; i < 3; ++i)
+            this.exchange(3 * a + i, 3 * b + i);
+    }
+
+    static mult(A, B, C) {
+        let res = [A.get(0) * B.get(0) + A.get(3) * B.get(1) + A.get(6) * B.get(2), A.get(1) * B.get(0) + A.get(4) * B.get(1) + A.get(7) * B.get(2),
+            A.get(2) * B.get(0) + A.get(5) * B.get(1) + A.get(8) * B.get(2),
+
+            A.get(0) * B.get(3) + A.get(3) * B.get(4) + A.get(6) * B.get(5), A.get(1) * B.get(3) + A.get(4) * B.get(4) + A.get(7) * B.get(5),
+            A.get(2) * B.get(3) + A.get(5) * B.get(4) + A.get(8) * B.get(5),
+
+            A.get(0) * B.get(6) + A.get(3) * B.get(7) + A.get(6) * B.get(8), A.get(1) * B.get(6) + A.get(4) * B.get(7) + A.get(7) * B.get(8),
+            A.get(2) * B.get(6) + A.get(5) * B.get(7) + A.get(8) * B.get(8),
+        ];
+
+        C.set(res);
+    }
+
+    getString() {
+        let e = this._e;
+        return "/" + e[0] + "\t " + e[3] + "\t " + e[6] + "\\\n" + "|" + e[1] + "\t " + e[4] + "\t " + e[7] + "|\n" + "\\" + e[2] + "\t " + e[5] + "\t " + e[8] +
+            "/\n";
+    }
+
+    transpose() {
+        this.exchange(1, 3);
+        this.exchange(2, 6);
+        this.exchange(5, 7);
+        return this;
+    }
+
+    // v * M = u
+    static VMmult(v, M, u) {
+        u[0] = M.get(0) * v[0] + M.get(1) * v[1] + M.get(2) * v[2];
+        u[1] = M.get(3) * v[0] + M.get(4) * v[1] + M.get(5) * v[2];
+        u[2] = M.get(6) * v[0] + M.get(7) * v[1] + M.get(8) * v[2];
     }
 
 
--- tests/Guess_tests.js
+++ tests/Guess_tests.js
@@ -51,14 +51,70 @@ describe('PSLQ Matrix', function() {
 			pslqMat.exchange(0,1);
 			assert(compArr(pslqMat._e, [ 4, 1, 7, 2, 5, 8, 3, 6, 9 ]));
     	});
+
+    	it('swapRow', function() {
+			pslqMat.swRow(0,2);
+			assert(compArr(pslqMat._e, [3, 6, 9, 2, 5, 8, 4, 1, 7, ]));
+    	});
+
+    	it('swapCol', function() {
+			pslqMat.swCol(0,2);
+			assert(compArr(pslqMat._e, [ 3, 2, 1, 6, 5, 4, 9, 8, 7 ]));
+    	});
+
+    	it('getRow', function() {
+			assert(compArr(pslqMat.getRow(0), [ 1,2,3 ]));
+			assert.deepEqual(pslqMat.getRow(0), [1,2,3]);
+    	});
+
+        it('set 1-ary', function(){
+			pslqMat.set([5,5,5,5,5,5,5,5,5]);
+			assert(compArr(pslqMat._e, [ 5,5,5,5,5,5,5,5 ]));
+        })
+
+        it('set 2-ary', function(){
+			pslqMat.set(0,10);
+			pslqMat.set(8,10);
+			assert(compArr(pslqMat._e, [ 10, 4, 7, 2, 5, 8, 3, 6, 10 ]));
+        })
 	});
 
-	describe('invert', function(){
+
+	describe('linear algebra', function(){
     	it('invert', function() {
 			let init = [[1,0,0], [0,5,0], [0,0,8]];
     	    pslqMat = new PSLQMatrix(init);
 			pslqMat.invert();
 			assert(compArr(pslqMat._e, [ 1, 0, 0, 0.2, 0, 0, 0, 0, 0.125 ]));
+    	});
+
+    	it('mult', function() {
+			let initA = [[1,2,3], [4,5,6], [7,8,9]];
+			let initB = [[9,8,7], [6,5,4], [3,2,1]];
+    	    let matA= new PSLQMatrix(initA);
+    	    let matB = new PSLQMatrix(initB);
+    	    let matC = new PSLQMatrix();
+            PSLQMatrix.mult(matA, matB, matC)
+
+			assert(compArr(matC._e, [30, 84, 138, 24, 69, 114, 18, 54, 90]));
+    	});
+
+    	it('transpose', function() {
+			let init = [[1,2,3], [4,5,6], [7,8,9]];
+    	    let mat = new PSLQMatrix(init);
+            mat.transpose();
+
+			assert.deepEqual(mat._e, [1,2,3,4,5,6,7,8,9]);
+    	});
+
+    	it('VMmult', function() {
+			let init = [[1,2,3], [4,5,6], [7,8,9]];
+    	    let mat = new PSLQMatrix(init);
+            let vec = [1,2,3];
+            let u = [];
+            PSLQMatrix.VMmult(vec, mat, u)
+
+			assert.deepEqual(u, [30 , 36, 42]);
     	});
 	});
 
