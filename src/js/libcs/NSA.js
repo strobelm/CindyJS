fe402a9a7dce1f46dde9a65f9c54028cbcd305f7
Michael Strobel <mstrobel@biodataanalysis.de>
2019-05-15 16:54:21 +0200

    introduce to build system and beautify

--- src/js/libcs/NSA.js
+++ src/js/libcs/NSA.js
@@ -5,22 +5,22 @@ LCOptions.eps = 1e-8;
 // Coefficients of LC numbers: [a,q]
 class LCCoeff {
     constructor(a, q) {
-	this.a = a;
-	this.q = q;
+        this.a = a;
+        this.q = q;
     }
 }
 
 class LCNumber {
     constructor(coeffArr) {
-	this.coeff = {};
+        this.coeff = {};
 
-	let minIdx = Infinity;
-	for (let el of coeffArr) {
-	    if (Math.abs(el.a) < LCOptions.eps) continue;
-	    if (el.q < minIdx) minIdx = el.q;
-	    // toString implicitely called
-	    this.coeff[el.q] = el.a;
-	}
-	this.normalize(minIdx);
+        let minIdx = Infinity;
+        for (let el of coeffArr) {
+            if (Math.abs(el.a) < LCOptions.eps) continue;
+            if (el.q < minIdx) minIdx = el.q;
+            // toString implicitely called
+            this.coeff[el.q] = el.a;
+        }
+        this.normalize(minIdx);
     }
 }
