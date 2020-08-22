80acbe31cfaa684fabd444f09e0c21cb3fff8d53
Michael Strobel <m.strobel@posteo.de>
2020-08-22 21:57:08 +0200

    ESLint: init

---
+++ Administration/eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 6
+    },
+    "rules": {
+        "no-undef": ["off"],
+        "no-unused-vars": ["off"],
+        "no-prototype-builtins": ["off"]
+    },
+};
